756ea682bcef0fc1d7dcd389366f192935932510
zariuz <nechetov@hotmail.com>
2021-04-25 09:27:23 +0300

    add pagination

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -3,19 +3,29 @@ import './main.less';
 import {useDispatch, useSelector} from 'react-redux';
 import {getRepos} from '../../actions/repos';
 import Repo from '../repo/Repo';
+import {setCurrentPage} from '../../reducers/reposReducer';
+import {createPages} from '../../utils/pagesCreator';
 
 const Main = () => {
   const dispatch = useDispatch();
   const repos = useSelector((state) => state.repos.items);
   const isFetching = useSelector((state) => state.repos.isFetching);
+  const currentPage = useSelector((state) => state.repos.currentPage);
+  const totalCount = useSelector((state) => state.repos.totalCount);
+  const perPage = useSelector((state) => state.repos.perPage);
   const [searchValue, setSearchValue] = useState('');
+  const pagesCount = Math.ceil(totalCount / perPage);
+  const pages = [];
+
+  createPages(pages, pagesCount, currentPage);
 
   useEffect(() => {
-    dispatch(getRepos());
-  }, []);
+    dispatch(getRepos(searchValue, currentPage, perPage));
+  }, [currentPage]);
 
   function searchHandler() {
-    dispatch(getRepos(searchValue));
+    dispatch(setCurrentPage(1));
+    dispatch(getRepos(searchValue, currentPage, perPage));
   }
 
   return (
@@ -38,6 +48,17 @@ const Main = () => {
       ) : (
         <div className="fetching"></div>
       )}
+
+      <div className="pages">
+        {pages.map((page, index) => (
+          <span
+            key={index}
+            className={currentPage == page ? 'current-page' : 'page'}
+            onClick={() => dispatch(setCurrentPage(page))}>
+            {page}
+          </span>
+        ))}
+      </div>
     </div>
   );
 };
